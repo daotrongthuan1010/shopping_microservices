530cac31ea26c4b32d829eb8be66b4b0583faca9
dtthuan3 <dtthuan3@cmc.com.vn>
2025-04-01 14:56:52 +0700

    Update code

--- Gateway-Service/src/main/java/com/example/gatewayservice/config/Config.java
+++ Gateway-Service/src/main/java/com/example/gatewayservice/config/Config.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigB
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class Config {
@@ -17,5 +19,17 @@ public class Config {
                         .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()).build()
         );
 
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/**").authenticated() // Bảo vệ các endpoint /api/**
+                        .anyExchange().permitAll()              // Cho phép các request khác
+                )
+                .oauth2Login(); // Sử dụng OAuth 2.0 Login với Okta
+
+        return http.build();
     }
 }
